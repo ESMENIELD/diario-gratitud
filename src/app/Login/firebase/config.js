b273808c0eae2347ee7e2ba8019442fad89d2c77
ESMENIELD <esmeralda.nield@alu.ing.unlp.edu.ar>
2025-06-12 10:22:34 -0300

    Ocultar credenciales de Firebase con .env.local

--- src/app/Login/firebase/config.js
+++ src/app/Login/firebase/config.js
@@ -1,12 +1,11 @@
-// src/lib/firebaseConfig.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCM_i_wPtOrxfSjWpkmZTpU5WaAZyFC328",
-  authDomain: "gratitude journaling",
-  projectId: "gratitude-journaling",
-  appId: "401338066886"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
